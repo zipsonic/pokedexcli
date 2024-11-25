575981a56e9ef69ffe2f9469bd42488ef6cedcc6
zipsonic <rick@hail3.com>
2024-11-24 19:23:27 -0800

    Initial loop and command entry.

--- main.go
+++ main.go
@@ -6,20 +6,47 @@ import (
 	"os"
 )
 
-type commands struct {
+type Command struct {
 	name string
 	desc string
-	cmd  func() error
+	cmd  func()
+}
+
+var cliCommand = map[string]Command{
+	"help": {
+		name: "help",
+		desc: "Prints this help screen",
+		cmd:  cmdHelp,
+	},
+	"exit": {
+		name: "exit",
+		desc: "Exits the Pokedex",
+		cmd:  cmdExit,
+	},
+}
+
+func cmdHelp() {
+	fmt.Println("Command Help Found")
+}
+
+func cmdExit() {
+	fmt.Println("Command Exit found")
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Println("Welcome to the Pokedex!")
+	fmt.Print("Pokedex >")
 
 	for scanner.Scan() {
-		fmt.Println("Pokedex >")
+
 		cmdInput := scanner.Text()
 
+		if cmdExec, ok := cliCommand[cmdInput]; ok {
+
+			cmdExec.cmd()
+		}
+		fmt.Print("Pokedex >")
 	}
 }
